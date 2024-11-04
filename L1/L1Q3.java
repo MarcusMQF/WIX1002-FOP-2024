373d111f1a4bd356f6e8214fd8445c074678142d
Marcus <marcusmah6969@gmail.com>
2024-11-05 00:09:48 +0800

    L1 Answers

---
+++ L1/L1Q3.java
@@ -0,0 +1,15 @@
+package L1;
+
+public class L1Q3 {
+    public static void main(String[] args) {
+        System.out.println("*********\t   ***   \t  *  \t    *    ");
+        System.out.println("*       *\t *     * \t *** \t   * *   ");
+        System.out.println("*       *\t*       *\t*****\t  *   *  ");
+        System.out.println("*       *\t*       *\t  *  \t *     * ");
+        System.out.println("*       *\t*       *\t  *  \t*       *");
+        System.out.println("*       *\t*       *\t  *  \t *     * ");
+        System.out.println("*       *\t*       *\t  *  \t  *   *  ");
+        System.out.println("*       *\t *     * \t  *  \t   * *   ");
+        System.out.println("*********\t   ***   \t  *  \t    *    ");
+    }
+}
---
+++ L1/L1Q4.java
@@ -0,0 +1,19 @@
+package L1;
+
+public class L1Q4 {
+    public static void main(String[] args) {
+        System.out.println("4. Create a new project named L1Q4. The project will display a chart (bar chart / line chart / pie chart) based on data in the table below. A chart is a graphical representation of data.\n");
+        
+        System.out.println("Table 1: Total Sales of Product A");
+        System.out.println("+----------------+--------+");
+        System.out.println("|      Month     | Amount |");
+        System.out.println("+----------------+--------+");
+        System.out.println("| January 2016   |  2500  |");
+        System.out.println("| February 2016  |  1600  |");
+        System.out.println("| March 2016     |  2000  |");
+        System.out.println("| April 2016     |  2700  |");
+        System.out.println("| May 2016       |  3200  |");
+        System.out.println("| June 2016      |   800  |");
+        System.out.println("+----------------+--------+");
+    }
+}
