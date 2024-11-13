6a151a818feaa58ae22c3a0fabe3b8c8314786b3
Marcus <marcusmah6969@gmail.com>
2024-11-13 15:27:24 +0800

    Modified Error

--- Viva 1/src/vivaQ3.java
+++ Viva 1/src/vivaQ3.java
@@ -4,118 +4,102 @@ public class vivaQ3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num;
+        boolean isPrime = true;
+        int count = 0;
+        int factorSum = 0;
+        long product = 1L;
 
-        while(true){
+        while (true) {
             System.out.print("Enter a positive number greater than 1: ");
             num = scanner.nextInt();
 
-            if(num > 1){
+            if (num > 1) {
                 break;
-            }else{
+            } else {
                 System.out.println("The input should be a positive integer greater than 1.");
             }
         }
 
-        boolean result = isPrime(num);
-
-        if(result){
-            System.out.println(num + " is a prime number.");
-        }else{
-            int factorCount = countFactors(num);
-            System.out.println("Integer is not a prime number, it has " + factorCount + " factors");
-            long factorSum = calculate(num);
-            Perfect(num, factorSum);
-            DisplayPrime(num);
+        // Count factors first
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
         }
 
-        scanner.close();
-    }
-
-    public static boolean isPrime(int num){
-        boolean isPrime = true;
-
-        for(int i = 2; i <= Math.sqrt(num); i++){
-            if(num % i == 0){
+        // Check if number is prime
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
                 isPrime = false;
                 break;
             }
         }
-        
-        return isPrime;
-    }
 
-    public static int countFactors(int num) {
-        int count = 0;
-        for(int i = 1; i <= num; i++) {
-            if(num % i == 0) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    public static long calculate(int num){
-        long factorSum = 0;
-        long factorProduct = 1;
-        boolean overflow = false;
-
-        System.out.print("The factors of this integer are: ");
-        boolean firstFactor = true;
-        for(int i = 1; i <= num; i++){
-            if(num % i == 0){
-                if (!firstFactor) {
-                    System.out.print(", ");
-                }
-                System.out.print(i);
-                firstFactor = false;
-                factorSum += i;
-                if(factorProduct <= Integer.MAX_VALUE / i){
-                    factorProduct *= i;
-                }else{
-                    overflow = true;
+        if (isPrime) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            // Print factor count first
+            System.out.println(num + " is not a prime number, it has " + count + " factors");
+            
+            // Then print the factors
+            System.out.print("The factors of this integer are: ");
+            boolean firstFactor = true;
+            for (int i = 1; i <= num; i++) {
+                if (num % i == 0) {
+                    if (!firstFactor) {
+                        System.out.print(", ");
+                    }
+                    System.out.print(i);
+                    firstFactor = false;
+                    factorSum += i;
+                    
+                    // Check if multiplication will exceed Long.MAX_VALUE
+                    if (product != 0 && i != 0 && product > Integer.MAX_VALUE / i) {
+                        product = 0; // Use 0 to indicate overflow
+                    } else {
+                        product *= i;
+                    }
                 }
             }
-        }
+            System.out.println();
 
-        System.out.println();
-        System.out.println("The sum of the factors is " + factorSum);
+            System.out.println("The sum of the factors is " + factorSum);
+            
+            if (product > Integer.MAX_VALUE || product == 0) {
+                System.out.println("The product of the factors is too large to display");
+            } else {
+                System.out.println("The product of the factors is " + product);
+            }
 
-        if(overflow || factorProduct > Integer.MAX_VALUE){
-            System.out.println("The product of the factors is too large to display");
-        }else{
-            System.out.println("The product of the factors is " + factorProduct);
-        }
+            // Check if perfect number
+            if (factorSum - num == num) {
+                System.out.println(num + " is a perfect number.");
+            } else {
+                System.out.println(num + " is not a perfect number.");
+            }
 
-        return factorSum;
-    }
-
-    public static void Perfect(int num, long factorSum){
-        if(factorSum - num == num){
-            System.out.println(num + " is a perfect number.");
-        }else{
-            System.out.println(num + " is not a perfect number.");
-        }
-    }
-
-    public static void DisplayPrime(int num){
-        System.out.print("Prime numbers between 2 and " + num + ": ");
-        boolean firstPrime = true;
-        for(int i = 2; i <= num; i++){
-            boolean isPrime = true;
-            for(int j = 2; j <= Math.sqrt(i); j++){
-                if(i % j == 0){
-                    isPrime = false;
-                    break;
+            // Print prime numbers between 2 and num
+            System.out.print("Prime numbers between 2 and " + num + ": ");
+            boolean firstPrime = true;
+            for (int i = 2; i <= num; i++) {
+                boolean currentIsPrime = true;
+                for (int j = 2; j <= Math.sqrt(i); j++) {
+                    if (i % j == 0) {
+                        currentIsPrime = false;
+                        break;
+                    }
+                }
+                if (currentIsPrime) {
+                    if (!firstPrime) {
+                        System.out.print(", ");
+                    }
+                    System.out.print(i);
+                    firstPrime = false;
                 }
             }
-            if(isPrime){
-                if (!firstPrime) {
-                    System.out.print(", ");
-                }
-                System.out.print(i);
-                firstPrime = false;
-            }
+            System.out.println();
         }
-        System.out.println();
+
+        scanner.close();
     }
 }
